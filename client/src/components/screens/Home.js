1270148031fedc96d3a48f20bffd494476cea448
Varun <varutalk@gmail.com>
2021-07-06 17:08:37 +0530

    home component

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,6 +1,56 @@
-const Home=()=>{
-    return(
-        <div>Home</div>
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="card home-card">
+                <h5>Varun</h5>
+                <div className="card-image">
+                    <img src="https://www.marshallsindia.com/images/younique/nature/nature-cover-slider-8.jpg" alt="" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons red-text">favorite</i>
+                    <h6>first post</h6>
+                    <p>this is my first post</p>
+                    <input placeholder="comment here..." type="text" ></input>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Varun</h5>
+                <div className="card-image">
+                    <img src="https://www.marshallsindia.com/images/younique/nature/nature-cover-slider-8.jpg" alt="" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons red-text">favorite</i>
+                    <h6>first post</h6>
+                    <p>this is my first post</p>
+                    <input placeholder="comment here..." type="text" ></input>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Varun</h5>
+                <div className="card-image">
+                    <img src="https://www.marshallsindia.com/images/younique/nature/nature-cover-slider-8.jpg" alt="" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons red-text">favorite</i>
+                    <h6>first post</h6>
+                    <p>this is my first post</p>
+                    <input placeholder="comment here..." type="text" ></input>
+                </div>
+            </div>
+            <div className="card home-card">
+                <h5>Varun</h5>
+                <div className="card-image">
+                    <img src="https://www.marshallsindia.com/images/younique/nature/nature-cover-slider-8.jpg" alt="" />
+                </div>
+                <div className="card-content">
+                    <i class="material-icons red-text">favorite</i>
+                    <h6>first post</h6>
+                    <p>this is my first post</p>
+                    <input placeholder="comment here..." type="text" ></input>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
