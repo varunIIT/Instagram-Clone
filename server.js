bec08dfb07522df89e8a711ab11f90c55203efb5
Varun <varutalk@gmail.com>
2021-07-16 15:42:43 +0530

    path.join to path.resolve

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if(process.env.NODE_ENV=='production'){
     app.use(express.static('client/build'))
     const path=require('path')
     app.get('*',(req,res)=>{
-        res.sendFile(path.join(__dirname,'client','build','index.html'))
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
 
